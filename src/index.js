57de6b0de3974f491b61ff6e10b76cc0621376c4
vivekworldindia <vivekchoudharyup9120@gmail.com>
2026-04-25 14:37:51 +0100

    user and video add

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {app} from "./app.js"
 
 // dotenv.config();
 dotenv.config({
     path: "./.env"
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 4000,()=>{
+        console.log(`server is running on port ${process.env.PORT ||4000}`)
+    })
 
-
-
+})
+.catch((err)=>{
+    console.log("Mongodb connection error",err)
+})
 // import express from "express";
 
 // const app = express();
